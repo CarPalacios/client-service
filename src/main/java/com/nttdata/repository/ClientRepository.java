c67cdc294984ace47b626a0a157f4d71d045c384
Carlos Alberto Palacios Aguado <cpalacag@emeal.nttdata.com>
2021-11-29 16:44:44 -0500

    Segundo Commit

--- src/main/java/com/nttdata/repository/ClientRepository.java
+++ src/main/java/com/nttdata/repository/ClientRepository.java
@@ -5,12 +5,7 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 //@NoRepositoryBean
 /** El repositorio client hace una herencia a ReactiveMongoRepository. */
-public interface ClientRepository extends ReactiveMongoRepository<Client, String> {
-
-//  crud    Metodo    Status Code
-//  create    post    201 create
-//  read    get      200 ok
-//  update    put      200 ok
-//  delete    delete    204 no content
+public interface ClientRepository 
+extends ReactiveMongoRepository<Client, String> {
   
 }
--- src/main/java/com/nttdata/service/ClientService.java
+++ src/main/java/com/nttdata/service/ClientService.java
@@ -4,7 +4,5 @@ import com.nttdata.model.Client;
 
 /**La interfaz ClientService extiende con CrudService.*/
 public interface ClientService extends CrudService<Client, String> {
-  
-  
-  
+
 }
